8c54899595d1f199f348e7c70e7e0ed68bfece95
Jannsis Krija <Jannis.krija@icloud.com>
2025-05-15 22:17:42 +0200

    Refactor code structure for improved readability and maintainability

--- src/main.js
+++ src/main.js
@@ -3,10 +3,24 @@ const path = require('path');
 
 let mainWindow;
 
+function getIconPath() {
+    switch (process.platform) {
+        case 'darwin':
+            return path.join(__dirname, '../IconikAIIcons/mac/icon.icns');
+        case 'win32':
+            return path.join(__dirname, '../IconikAIIcons/web/favicon.ico');
+        case 'linux':
+            return path.join(__dirname, '../IconikAIIcons/web/favicon-32x32.png');
+        default:
+            return path.join(__dirname, '../IconikAIIcons/web/favicon-32x32.png');
+    }
+}
+
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1500,
         height: 900,
+        icon: getIconPath(),
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
@@ -19,6 +33,9 @@ function createWindow() {
 
 app.on('ready', () => {
     createWindow();
+    if (process.platform === 'darwin') {
+        app.dock.setIcon(path.join(__dirname, '../IconikAIIcons/ios/appstore.png'));
+    }
 });
 
 app.on('window-all-closed', () => {
